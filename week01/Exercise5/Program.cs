1ead4032d464a1c4ddc53fe35aeb33de3bd0b234
unknown <henricokayz@gmail.com>
2026-03-13 16:27:06 +0300

    Updated program and added test file

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
+    // Function 1: DisplayWelcome
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function 2: PromptUserName
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Function 3: PromptUserNumber
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+        return number;
+    }
+
+    // Function 4: SquareNumber
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    // Function 5: DisplayResult
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call each function in order
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squared = SquareNumber(userNumber);
+
+        DisplayResult(userName, squared);
     }
-}
+}   
+    
