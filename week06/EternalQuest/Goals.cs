3f6a1d964e430eed86963001c7f7933063662173
unknown <henricokayz@gmail.com>
2026-04-11 23:23:59 +0300

    W06 Eternal Quest Program - complete with creativity features

---
+++ week06/EternalQuest/Goals.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public string GetName() => _shortName;
+    public int GetPoints() => _points;
+
+    public abstract int RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string GetStringRepresentation();
+
+    public virtual string GetDetailsString()
+    {
+        string checkbox = IsComplete() ? "[X]" : "[ ]";
+        return $"{checkbox} {_shortName} ({_description})";
+    }
+}
